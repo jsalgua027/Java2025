c9afbf5605a1d97c008711a31df4ff5c4d90fd28
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2025-03-04 18:00:22 +0100

    nuevas clases con explicaciones

---
+++ PrimerosPasos/src/UsoConstantes.java
@@ -0,0 +1,17 @@
+
+public class UsoConstantes {
+	
+	//constante de clase
+	
+	private static final double PI=3.1416;  //en mayuscula
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 final int ejemplo=9;  // se puede inicializar en otra linea pero las de clase tienen que estar inicializadas en la misma linea
+		 
+		System.out.println(ejemplo);
+		
+	}
+
+}
--- PrimerosPasos/src/UsoVariables.java
+++ PrimerosPasos/src/UsoVariables.java
@@ -5,14 +5,23 @@ public class UsoVariables {
 		// TODO Auto-generated method stub
 
 		byte edad; //declarion de variables
-		
 		edad=17; //inicializacion de varibles
-		
 		byte edad2=18;
 		
-		System.out.println(edad);
+		System.out.println(edad); //el println hace el salto de linea
+		System.out.println(edad2);
+					
+		// declaraciones multiples y constantes
+		System.out.println("**************************declaraciones multiples************************");
+		int operador1, operador2, resultado;
 		
-		System.out.print(edad2);
+		operador1=7;
+		operador2=9;
+		resultado=operador1+operador2;
+		System.out.println(resultado);
+		
+		int num1=10,num2=10,res=num1+num2;
+		System.out.print(res);
 		
 		
 	}
